34b44f76adedb97f61654b8389c60bbd519abfc6
mbarbon <mbarbon@2e9104e8-d5c8-4c90-bb67-bfd1792b7337>
2005-08-17 19:27:46 +0000

    Support for choosing the desired configuration from Alien::wxWidgets. Requires Alien::wxWidgets 0.03.

--- script/fix_alien_path.pl
+++ script/fix_alien_path.pl
@@ -5,16 +5,22 @@
 ## Author:      Mattia Barbon
 ## Modified by:
 ## Created:     15/08/2005
-## RCS-ID:      $Id: fix_alien_path.pl,v 1.1.2.2 2005/08/16 20:56:56 mbarbon Exp $
+## RCS-ID:      $Id: fix_alien_path.pl,v 1.1.2.3 2005/08/17 19:27:46 mbarbon Exp $
 ## Copyright:   (c) 2005 Mattia Barbon
 ## Licence:     This program is free software; you can redistribute it and/or
 ##              modify it under the same terms as Perl itself
 #############################################################################
 
 use strict;
-use Alien::wxWidgets 0.02;
+use blib; # to pick the correct 'Wx::build::Options'
+use Alien::wxWidgets 0.03 ();
+use Wx::build::Options;
 use Fatal qw(open close unlink);
 use Config;
+
+# we do not care about the options, just that Alien::wxWidgets
+# initialized with the correct key
+Wx::build::Options->get_makemaker_options( 'saved' );
 
 my( $from, $to ) = @ARGV;
 my $path = Alien::wxWidgets->shared_library_path;
