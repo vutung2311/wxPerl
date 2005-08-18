70fd910a8ec499a38ca946929424d5b0b462e745
mbarbon <mbarbon@2e9104e8-d5c8-4c90-bb67-bfd1792b7337>
2005-08-18 16:12:24 +0000

    Prepare release 0.25_01 from the Alien branch.

--- ext/pperl/splashfast/SplashFast.pm
+++ ext/pperl/splashfast/SplashFast.pm
@@ -4,7 +4,7 @@
 ## Author:      Graciliano M. P.
 ## Modified by:
 ## Created:     30/06/2002
-## RCS-ID:      $Id: SplashFast.pm,v 1.21.2.1 2005/08/15 16:17:14 mbarbon Exp $
+## RCS-ID:      $Id: SplashFast.pm,v 1.21.2.2 2005/08/18 16:12:24 mbarbon Exp $
 ## Copyright:   (c) 2002-2005 Graciliano M. P.
 ## Licence:     This program is free software; you can redistribute it and/or
 ##              modify it under the same terms as Perl itself
@@ -60,9 +60,11 @@ sub OnInit { return 1 }
 
 package Wx ;
 
-use vars qw($VERSION);
+use vars qw($VERSION $XS_VERSION);
 
-$VERSION = '0.26'; # bootstrap will catch wrong versions
+$VERSION = '0.25_01'; # bootstrap will catch wrong versions
+$XS_VERSION = $VERSION;
+$VERSION = eval $VERSION;
 
 sub wxPL_STATIC();
 sub _wx_boot($$) {
@@ -85,9 +87,9 @@ sub _wx_boot($$) {
   }
 }
 
-_wx_boot( 'Wx', $VERSION );
-_boot_Constant( 'Wx', $VERSION );
-_boot_GDI( 'Wx', $VERSION );
+_wx_boot( 'Wx', $XS_VERSION );
+_boot_Constant( 'Wx', $XS_VERSION );
+_boot_GDI( 'Wx', $XS_VERSION );
 
 Load();
 
