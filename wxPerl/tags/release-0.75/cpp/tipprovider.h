49d689d00aafd170a55e0353a49c863ae522361a
mbarbon <mbarbon@2e9104e8-d5c8-4c90-bb67-bfd1792b7337>
2007-07-08 20:47:37 +0000

    Tagging reelase 0.75.

---
+++ wxPerl/tags/release-0.75/cpp/tipprovider.h
@@ -0,0 +1,37 @@
+/////////////////////////////////////////////////////////////////////////////
+// Name:        cpp/tipprovider.h
+// Purpose:     C++ wrapper for wxTipProvider
+// Author:      Mattia Barbon
+// Modified by:
+// Created:     07/09/2001
+// RCS-ID:      $Id$
+// Copyright:   (c) 2001-2002 Mattia Barbon
+// Licence:     This program is free software; you can redistribute it and/or
+//              modify it under the same terms as Perl itself
+/////////////////////////////////////////////////////////////////////////////
+
+#include <wx/tipdlg.h>
+
+class wxPliTipProvider:public wxTipProvider
+{
+    WXPLI_DECLARE_V_CBACK();
+public:
+    wxPliTipProvider( const char* package, size_t currentTip )
+        :wxTipProvider( currentTip ),
+         m_callback( "Wx::TipProvider" )
+    {
+        dTHX;
+        m_callback.SetSelf( wxPli_non_object_2_sv( aTHX_ sv_newmortal(), this,
+                                                   package ) );
+    }
+
+    void SetCurrentTip( size_t currentTip ) { m_currentTip = currentTip; }
+
+    DEC_V_CBACK_WXSTRING__VOID( GetTip );
+};
+
+DEF_V_CBACK_WXSTRING__VOID_pure( wxPliTipProvider, wxTipProvider, GetTip );
+
+// Local variables: //
+// mode: c++ //
+// End: //
