f26cfe2fe537cddbea3f5dab6df5315733edcabd
mbarbon <mbarbon@2e9104e8-d5c8-4c90-bb67-bfd1792b7337>
2010-02-21 15:24:17 +0000

    Handle both abstract classes and classes that can be subclassed to override methods but do not need to.   Handle non-pure virtual methods.   Handle classes with default constructors.   Misc fixes/additions.

--- build/Wx/Virtual/XSP.pm
+++ build/Wx/Virtual/XSP.pm
@@ -20,7 +20,12 @@ sub register_plugin {
 sub handle_method_tag {
     my( $self, $method, $tag, %args ) = @_;
 
-    $self->{virtual_methods}{$method} = $method;
+    if(    $args{any_positional_arguments}
+        && $args{any_positional_arguments}[0] eq 'pure' ) {
+        $self->{virtual_methods}{$method} = [ $method, 1 ];
+    } else {
+        $self->{virtual_methods}{$method} = [ $method, 0 ];
+    }
 
     1;
 }
@@ -34,6 +39,11 @@ my %type_map =
                             default_value  => '0',
                             type_char      => 'i',
                             },
+    # TODO merge
+    'wxAlignment'      => { convert_return => '(wxAlignment)SvIV( ret )',
+                            default_value  => '(wxAlignment)0',
+                            type_char      => 'i',
+                            },
     'unsigned int'     => { convert_return => 'SvUV( ret )',
                             default_value  => '0',
                             type_char      => 'I',
@@ -43,16 +53,34 @@ my %type_map =
                             type_char      => 'P',
                             arguments      => '&%s',
                             },
+    # TODO merge
+    'const wxString&'  => { convert_return => 'wxPli_sv_2_wxString( aTHX_ ret )',
+                            default_value  => 'wxEmptyString',
+                            type_char      => 'P',
+                            arguments      => '&%s',
+                            },
     'wxVariant&'       => { convert_return => 'wxPli_sv_2_variant( aTHX_ ret )',
                             default_value  => 'wxVariant()',
                             type_char      => 'q',
                             arguments      => '&%s, "Wx::Variant"',
                             },
+    # TODO merge
     'const wxVariant&' => { convert_return => 'wxPli_sv_2_variant( aTHX_ ret )',
                             default_value  => 'wxVariant()',
                             type_char      => 'q',
                             arguments      => '&%s, "Wx::Variant"',
                             },
+    'wxBitmap'        => { convert_return => '*(wxBitmap*)wxPli_sv_2_object( aTHX_ ret, "Wx::Bitmap" )',
+                           default_value  => 'wxBitmap()',
+                           type_char      => 'O',
+                           arguments      => '&%s',
+                           },
+    # TODO merge
+    'const wxBitmap&' => { convert_return => '*(wxBitmap*)wxPli_sv_2_object( aTHX_ ret, "Wx::Bitmap" )',
+                           default_value  => 'wxBitmap()',
+                           type_char      => 'O',
+                           arguments      => '&%s',
+                           },
     );
 
 sub _virtual_typemap {
@@ -73,23 +101,34 @@ sub post_process {
         next unless $node->isa( 'ExtUtils::XSpp::Node::Class' );
         next if $self->{virtual_classes}{$node};
         my @virtual;
+        my $abstract_class;
 
         foreach my $method ( @{$node->methods} ) {
             next unless $self->{virtual_methods}{$method};
 
-            push @virtual, $method;
+            push @virtual, $self->{virtual_methods}{$method};
+            $abstract_class ||= $virtual[-1][1];
         }
 
         next unless @virtual;
 
         # TODO wxPerl-specific
         ( my $cpp_class = $node->cpp_name ) =~ s/^wx/wxPl/;
-        ( my $perl_class = $cpp_class ) =~ s/^wx/Wx::/;
+        my $perl_class;
+        if( $abstract_class ) {
+            ( $perl_class = $cpp_class ) =~ s/^wx/Wx::/;
+        } else {
+            ( $perl_class = $cpp_class ) =~ s/^wxPl/Wx::/;
+        }
         my $file = lc "cpp/$cpp_class.h";
 
-        unshift @$nodes,
-                ExtUtils::XSpp::Node::Raw->new
-                    ( rows => [ "#include \"$file\"" ] );
+        my $include = ExtUtils::XSpp::Node::Raw->new
+                          ( rows => [ "#include \"$file\"" ] );
+        for( my $i = 0; $i <= $#$nodes; ++$i ) {
+            next unless $nodes->[$i] == $node;
+            splice @$nodes, $i, 0, $include;
+            last;
+        }
 
         # for abstract class, delete all constructors
         my @constructors = grep $_->isa( 'ExtUtils::XSpp::Node::Constructor' ),
@@ -98,6 +137,8 @@ sub post_process {
         $node->delete_methods( @constructors );
 
         push @cpp_code, sprintf <<EOC,
+#include "cpp/v_cback.h"
+
 class %s : public %s
 {
     // TODO wxPerl-specific
@@ -110,29 +151,34 @@ EOC
         foreach my $constructor ( @constructors ) {
             my $cpp_parms = join ', ', map $_->name, @{$constructor->arguments};
             my $cpp_args = join ', ', map $_->print, @{$constructor->arguments};
+            my $comma = @{$constructor->arguments} ? ',' : '';
 
             push @cpp_code, sprintf <<EOC,
-    %s( const char* CLASS, %s )
+    %s( const char* CLASS %s %s )
         : %s( %s ),
           m_callback( "%s" )
     {
         m_callback.SetSelf( wxPli_make_object( this, CLASS ), true );
     }
 EOC
-              $cpp_class, $cpp_args, $node->cpp_name, $cpp_parms, $perl_class;
+              $cpp_class, $comma, $cpp_args, $node->cpp_name, $cpp_parms, $perl_class;
 
-            my $code = [ "RETVAL = new $cpp_class( CLASS, $cpp_parms );" ];
+            my $code = [ "RETVAL = new $cpp_class( CLASS $comma $cpp_parms );" ];
 
             my $new_ctor = ExtUtils::XSpp::Node::Constructor->new
                                ( cpp_name   => $cpp_class,
+                                 perl_name  => $constructor->perl_name,
                                  code       => $code,
                                  arguments  => $constructor->arguments,
+                                 postcall   => $constructor->postcall,
+                                 cleanup    => $constructor->cleanup,
                                  );
 
             push @new_constructors, $new_ctor;
         }
 
-        foreach my $method ( @virtual ) {
+        foreach my $m ( @virtual ) {
+            my( $method, $pure ) = @$m;
             my( @cpp_parms, @arg_types );
             foreach my $arg ( @{$method->arguments} ) {
                 my $typemap = _virtual_typemap( $arg->type );
@@ -155,8 +201,7 @@ EOC
             my $call_base = $node->cpp_name . '::' . $method->cpp_name .
               '(' . $cpp_parms . ')';
             if( $method->ret_type->is_void ) {
-                # TODO only pure methods for now
-                my $default = 'return';
+                my $default = $pure ? 'return' : $call_base;
                 push @cpp_code, sprintf <<EOT,
     // TODO wxPerl-specific
     {
@@ -173,9 +218,7 @@ EOT
                   $method->cpp_name, $arg_types, $cpp_parms, $default;
             } else {
                 my $ret_type_map = _virtual_typemap( $method->ret_type );
-
-                # TODO only pure for now
-                my $default = $ret_type_map->{default_value};
+                my $default = $pure ? $ret_type_map->{default_value} : $call_base;
                 my $convert = $ret_type_map->{convert_return};
                 push @cpp_code, sprintf <<EOT,
     // TODO wxPerl-specific
@@ -193,6 +236,26 @@ EOT
 EOT
                   $method->cpp_name, $arg_types, $cpp_parms, $convert, $default;
             }
+
+            my $callbase_decl = $method->ret_type->print . ' ' .
+                                'base_' . $method->cpp_name . '( ' .
+                                join( ', ', map $_->print, @{$method->arguments} ) . ')' .
+                                ( $method->const ? ' const' : '' );
+
+            if( !$pure ) {
+                push @cpp_code, '    ' . $callbase_decl, '    {';
+
+                if( $method->ret_type->is_void ) {
+                    push @cpp_code, '        ' . $call_base . ';';
+                } else {
+                    push @cpp_code, '        return ' . $call_base . ';';
+                }
+
+                push @cpp_code, '    }';
+
+#                 $method->set_access( 'public' );
+#                 $method->set_cpp_name( 'base_' . $method->cpp_name );
+            }
         }
 
         push @cpp_code, '};', '';
@@ -201,15 +264,19 @@ EOT
         print $h_file join "\n", @cpp_code;
         close $h_file;
 
-        ExtUtils::XSpp::Typemap::add_default_class_typemaps( $cpp_class );
-        my $new_class = ExtUtils::XSpp::Node::Class->new
-                            ( cpp_name     => $cpp_class,
-                              perl_name    => $perl_class,
-                              base_classes => [ $node ],
-                              methods      => \@new_constructors,
-                              );
+        ExtUtils::XSpp::Typemap::add_class_default_typemaps( $cpp_class );
+        if( $abstract_class ) {
+            my $new_class = ExtUtils::XSpp::Node::Class->new
+                                ( cpp_name     => $cpp_class,
+                                  perl_name    => $perl_class,
+                                  base_classes => [ $node ],
+                                  methods      => \@new_constructors,
+                                );
 
-        push @$nodes, $new_class;
+            push @$nodes, $new_class;
+        } else {
+            $node->add_methods( @new_constructors );
+        }
     }
 }
 
