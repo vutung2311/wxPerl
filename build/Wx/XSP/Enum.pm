3ce868e4edb6f2b8f6400c57beeb976cf02ce825
mbarbon <mbarbon@2e9104e8-d5c8-4c90-bb67-bfd1792b7337>
2010-04-22 21:36:12 +0000

    Guard the generated code to export enum values with compilation directives equivalent to the ones defined in the source XS++ code.

--- build/Wx/XSP/Enum.pm
+++ build/Wx/XSP/Enum.pm
@@ -14,24 +14,38 @@ sub register_plugin {
 sub post_process {
     my( $self, $nodes ) = @_;
     my $parser = $self->{parser};
-    my %constants;
+    my( %constants, %conditions );
 
     foreach my $node ( @$nodes ) {
         next unless $node->isa( 'ExtUtils::XSpp::Node::Enum' );
 
+        $conditions{$node->condition} ||= 1 if $node->condition;
         foreach my $val ( @{$node->elements} ) {
             next unless $val->isa( 'ExtUtils::XSpp::Node::EnumValue' );
-            $constants{$val->name} ||= 1;
+            $constants{$val->name} ||= [ $val->condition ];
+            $conditions{$val->condition} ||= 1 if $val->condition;
         }
     }
 
     ( my $name = File::Basename::basename( $parser->current_file ) ) =~ tr/./_/;
     my $file = "xspp/$name.h";
-    my $consts = join "\n",
-                  map "    r( $_ );",
-                      keys %constants;
+    my @defines;
+    while( my( $k, $v ) = each %constants ) {
+        if( $v->[0] ) {
+            push @defines, "#ifdef $v->[0]",
+                           "    r( $k );",
+                           "#endif",
+        } else {
+            push @defines, "    r( $k );",
+        }
+    }
+    my $consts = join "\n", @defines;
+    my $all_conditions = join ' && ', 1,
+                         map "defined( $_ )",
+                             keys %conditions;
+    my @lines = sprintf <<'EOT', $all_conditions, $name, $consts, $name, $name;
+#if %s
 
-    my @lines = sprintf <<'EOT', $name, $consts, $name, $name;
 #include "cpp/constants.h"
 
 static double %s_constant( const char* name, int arg )
@@ -58,6 +72,8 @@ static double %s_constant( const char* name, int arg )
 }
 
 static wxPlConstants %s_module( &%s_constant );
+
+#endif
 EOT
 
     push @$nodes,
